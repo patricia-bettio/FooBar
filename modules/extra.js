5bf4232330ab4b8e58c17aa953936193aaed7f37
megane <megane.londono@hotmail.com>
2020-05-16 13:11:55 +0200

    updated heroku link

--- modules/extra.js
+++ modules/extra.js
@@ -1,15 +1,16 @@
 //-----------------------------------convert all times--------------------------------------//
 
-export const urlApi = "https://beer-waffle.herokuapp.com/"; 
-export default function dateFormatter(timestamp){    
-//console.log(timestamp);
-let date = new Date(timestamp);
-let hours = date.getHours();
-// Minutes part from the timestamp
-let minutes = "0" + date.getMinutes();
-// Seconds part from the timestamp
-let seconds = "0" + date.getSeconds();
-let newTimeFormatted = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-//console.log(newTimeFormatted)
-return newTimeFormatted;
-} 
+export const urlApi = "https://beer-waffles.herokuapp.com/";
+export default function dateFormatter(timestamp) {
+  //console.log(timestamp);
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  // Minutes part from the timestamp
+  let minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  let seconds = "0" + date.getSeconds();
+  let newTimeFormatted =
+    hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+  //console.log(newTimeFormatted)
+  return newTimeFormatted;
+}
