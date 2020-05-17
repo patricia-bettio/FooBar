4c1ecfb96746c6cf2e04fd1d7fb111a81276a068
Patricia Bettio <pfbettio@gmail.com>
2020-05-17 20:01:20 +0200

    fix sort and filter. Images ok. Form structure on

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ import { urlDetails } from "./modules/extra";
 document.addEventListener("DOMContentLoaded", init);
 
 let allBeers = [];
+let ipaBeers = [];
+let alcNumber = [];
 
 function init() {
   fetchData();
+  
 }
 
 function fetchData() {
@@ -19,19 +22,27 @@ function fetchData() {
     
       fetchBeers(e);
       //fetchDetails(e);
-      setFilters(e);
+      
+      addToCart()
+      
+
             
     });
+
 }
 
 function fetchBeers(beers) {
-  console.log(beers);
   //make global:
+
   allBeers = beers;
-  console.log(allBeers)
+  //console.log(allBeers)
+  
+  setFilters(beers);
+  //console.log(ipaBeers)
+
   //show on template:
   beers.forEach(displayBeer);
- 
+  
 
 }
 
@@ -46,12 +57,7 @@ function displayBeer(beer) {
   beerClone.querySelector("h2.name").textContent = `${beer.name}`;
   beerClone.querySelector("h2.category").textContent = `${beer.category}`;
   beerClone.querySelector("p.alcohol").textContent = `ABV: ${beer.alc} %`;
-  console.log(beer.label);
-  console.log(`images/${beer.label}`);
   beerClone.querySelector(".logo").src = `images/${beer.label}`;
-  console.log(beerClone.querySelector(".logo").src);
-  //test
-  //document.querySelector(".testPNG").style.backgroundImage = `images/${beer.label}`
 
   //hidden details in template:
   beerClone.querySelector(".aroma").textContent = `Aroma: ${beer.description.aroma}`;
@@ -60,37 +66,92 @@ function displayBeer(beer) {
   beerClone.querySelector(".mouthfeel").textContent = `Mouthfeel: ${beer.description.mouthfeel}`;
   beerClone.querySelector(".overallImpression").textContent = `Overall Impression: ${beer.description.overallImpression}`;
   //grab by category
-  //const formatCategory = beer.category.toLowerCase().split(" ")[0];
-  //beerClone.querySelector(".showMore").src = `svg/glasses/${formatCategory}_glass.svg`;
+  
+  //beerClone.querySelector(".logo").src = `images/${beer.label}`;
+  const formatCategory = beer.category.toLowerCase().split(" ")[0];
+  let showMoreGlass = beerClone.querySelector(".showMore")
+  showMoreGlass.src = `svg/glasses/${formatCategory}_glass.svg`;
   //SHOW DETAILS
   let selectedBeer = beerClone.querySelector(".dropdown");
   selectedBeer.classList.add("hide");
   beerClone.querySelector(".showMore").addEventListener("click", (e) => {
+  //show deatils and turn glass onclick
   selectedBeer.classList.toggle("hide");
+  showMoreGlass.classList.toggle("rotate");
   });
+  //FORM
+  const form = beerClone.querySelector("form");
+  window.form = form;
+  //console.log(form)
+  const elements = form.elements;
+  window.elements = elements;
+  //console.log(elements);
+  //console.log(elements.quantity.value)
+  //console.log(form.checkValidity())
+  form.addEventListener("submit", addToCart);
+  //SORT
+  alcNumber = beer.alc;
 
+
+
+  
+ 
+  //append
   beerList.appendChild(beerClone);
 
 }
 
-function addToCart(addBeer) {
-  console.log(addBeer);
+//-------------------------------------- FORM--------------------------------------//
+function addToCart() {
+  //console.log("added")
+
+ 
 }
 
+//-------------------------------------- FILTER -------------------------------------//
 function setFilters(allBeers){
-console.log(allBeers)
-// filters
-document.querySelector("[data-filter='IPA']").addEventListener("click", filterIPA)
+  console.log(allBeers)
+  // filters event listeners:
+  document.querySelector("[data-filter='IPA']").addEventListener("click", filterIPA)
+  document.querySelector("[data-sort='alc']").addEventListener("click", sortAlc);
+
 }
 
 function filterIPA(){
   console.log(allBeers)
-  //console.log(allBeers.length)
-  let ipaBeers =  allBeers.filter(function(IPA) {
+  ipaBeers =  allBeers.filter(function(IPA) {
   return IPA.category == "IPA";
   });
+  //filter WORKS - check console
+  //missing: display ONLY the ipa category
   console.log(ipaBeers)
-  displayBeer(ipaBeers)
+  //displayBeer(ipaBeers)
+
+}
+
+function sortAlc(){
+console.log(allBeers)
+console.log(alcNumber)
+    if (event.target.dataset.sortDirection === "asc") {
+        event.target.dataset.sortDirection = "desc";
+        console.log("sort asc")
+        firstAsc();
+    } else {
+        console.log("sort desc")
+        firstDesc();
+        event.target.dataset.sortDirection = "asc";
+    }
+  
+}
+
+//condition - ascending
+function firstAsc(){
+  console.log(allBeers)
+
+ 
+}
+//condition - descending
+function firstDesc(){
 
 }
 
