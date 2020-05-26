1936e142ffbbba9f726646a2e0916312f5b2a820
megane <megane.londono@hotmail.com>
2020-05-26 14:18:29 +0200

    set up modals for edit and post

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function displaySingleBeer(beer) {
     console.log(amount);
   });
 
-  document.querySelector(".testPost").addEventListener("click", (e) => {
+  /* document.querySelector(".testPost").addEventListener("click", (e) => {
     e.preventDefault();
 
     let validForm = true;
@@ -93,7 +93,7 @@ function displaySingleBeer(beer) {
         amount: form.elements.quantity.value,
       });
     }
-  });
+  }); */
 
   //hidden details in template:
   beerClone.querySelector(
@@ -307,140 +307,156 @@ function firstDesc() {
 }
 
 // ------------ modal ---------- //
-function setUpModal() {
-  const modal = document.querySelector("#modal");
-  const modalBtn = document.querySelector("#modalBtn");
-  const span = document.querySelector(".closeModal");
+const modal = document.querySelector("#modal");
+const modalBtn = document.querySelector("#modalBtn");
+const span = document.querySelector(".closeModal");
+const editBtn = document.querySelector("#editBtn");
+const modal2 = document.querySelector("#modal2");
+const modalBtn2 = document.querySelector("#modalBtn2");
+const span2 = document.querySelector(".closeModal2");
 
+function setUpModal() {
   modalBtn.addEventListener("click", (e) => {
     modal.style.display = "block";
   });
   span.addEventListener("click", (e) => {
     modal.style.display = "none";
   });
+  //close modal when "edit" btn clicked
+  editBtn.addEventListener("click", (e) => {
+    modal.style.display = "none";
+  });
+  setUpPayment();
+}
 
-  //close modal when clicked outside of box
+// ------- payment modal ------ //
+function setUpPayment() {
+  modalBtn2.addEventListener("click", (e) => {
+    modal2.style.display = "block";
+  });
+  span2.addEventListener("click", (e) => {
+    modal2.style.display = "none";
+  });
   window.addEventListener("click", (e) => {
     if (event.target == modal) {
-      modal.style.display = "none";
+      modal2.style.display = "none";
     }
   });
   setUpForm();
-}
 
-// PAYMENT FORM
-function setUpForm() {
-  const body = document.querySelector("body");
-  window.form = form;
-  window.elements = elements;
-  const form = document.querySelector("form");
-  const elements = form.elements;
+  function setUpForm() {
+    const body = document.querySelector("body");
+    window.form = form;
+    window.elements = elements;
+    const form = document.querySelector("form");
+    const elements = form.elements;
 
-  form.setAttribute("novalidate", true);
+    form.setAttribute("novalidate", true);
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-    const formElements = form.querySelectorAll("input");
-    formElements.forEach((el) => {
-      el.classList.remove("invalid");
+      const formElements = form.querySelectorAll("input");
+      formElements.forEach((el) => {
+        el.classList.remove("invalid");
+      });
+
+      if (form.checkValidity()) {
+        body.classList.add("noForm");
+        body.classList.add(
+          "loadingGif"
+        ); /* &&
+            location.assign("http://localhost:1234/app.html") */
+      } else {
+        formElements.forEach((el) => {
+          if (!el.checkValidity()) {
+            el.classList.add("invalid");
+          }
+        });
+      }
     });
 
-    if (form.checkValidity()) {
-      body.classList.add("noForm");
-      body.classList.add(
-        "loadingGif"
-      ); /* &&
-        location.assign("http://localhost:1234/app.html") */
-    } else {
-      formElements.forEach((el) => {
-        if (!el.checkValidity()) {
-          el.classList.add("invalid");
-        }
-      });
-    }
-  });
+    // ------ code copied from https://codepen.io/murani/pen/KyVbrp --------
+    let ccNumberInput = document.querySelector(".cc-number-input"),
+      ccNumberPattern = /^\d{0,16}$/g,
+      ccNumberSeparator = " ",
+      ccNumberInputOldValue,
+      ccNumberInputOldCursor,
+      ccExpiryInput = document.querySelector(".cc-expiry-input"),
+      ccExpiryPattern = /^\d{0,4}$/g,
+      ccExpirySeparator = "/",
+      ccExpiryInputOldValue,
+      mask = (value, limit, separator) => {
+        var output = [];
+        for (let i = 0; i < value.length; i++) {
+          if (i !== 0 && i % limit === 0) {
+            output.push(separator);
+          }
 
-  // ------ code copied from https://codepen.io/murani/pen/KyVbrp --------
-  let ccNumberInput = document.querySelector(".cc-number-input"),
-    ccNumberPattern = /^\d{0,16}$/g,
-    ccNumberSeparator = " ",
-    ccNumberInputOldValue,
-    ccNumberInputOldCursor,
-    ccExpiryInput = document.querySelector(".cc-expiry-input"),
-    ccExpiryPattern = /^\d{0,4}$/g,
-    ccExpirySeparator = "/",
-    ccExpiryInputOldValue,
-    mask = (value, limit, separator) => {
-      var output = [];
-      for (let i = 0; i < value.length; i++) {
-        if (i !== 0 && i % limit === 0) {
-          output.push(separator);
+          output.push(value[i]);
         }
 
-        output.push(value[i]);
-      }
+        return output.join("");
+      },
+      unmask = (value) => value.replace(/[^\d]/g, ""),
+      checkSeparator = (position, interval) =>
+        Math.floor(position / (interval + 1)),
+      ccNumberInputKeyDownHandler = (e) => {
+        let el = e.target;
+        ccNumberInputOldValue = el.value;
+        ccNumberInputOldCursor = el.selectionEnd;
+      },
+      ccNumberInputInputHandler = (e) => {
+        let el = e.target,
+          newValue = unmask(el.value),
+          newCursorPosition;
 
-      return output.join("");
-    },
-    unmask = (value) => value.replace(/[^\d]/g, ""),
-    checkSeparator = (position, interval) =>
-      Math.floor(position / (interval + 1)),
-    ccNumberInputKeyDownHandler = (e) => {
-      let el = e.target;
-      ccNumberInputOldValue = el.value;
-      ccNumberInputOldCursor = el.selectionEnd;
-    },
-    ccNumberInputInputHandler = (e) => {
-      let el = e.target,
-        newValue = unmask(el.value),
-        newCursorPosition;
+        if (newValue.match(ccNumberPattern)) {
+          newValue = mask(newValue, 4, ccNumberSeparator);
 
-      if (newValue.match(ccNumberPattern)) {
-        newValue = mask(newValue, 4, ccNumberSeparator);
+          newCursorPosition =
+            ccNumberInputOldCursor -
+            checkSeparator(ccNumberInputOldCursor, 4) +
+            checkSeparator(
+              ccNumberInputOldCursor +
+                (newValue.length - ccNumberInputOldValue.length),
+              4
+            ) +
+            (unmask(newValue).length - unmask(ccNumberInputOldValue).length);
 
-        newCursorPosition =
-          ccNumberInputOldCursor -
-          checkSeparator(ccNumberInputOldCursor, 4) +
-          checkSeparator(
-            ccNumberInputOldCursor +
-              (newValue.length - ccNumberInputOldValue.length),
-            4
-          ) +
-          (unmask(newValue).length - unmask(ccNumberInputOldValue).length);
+          el.value = newValue !== "" ? newValue : "";
+        } else {
+          el.value = ccNumberInputOldValue;
+          newCursorPosition = ccNumberInputOldCursor;
+        }
 
-        el.value = newValue !== "" ? newValue : "";
-      } else {
-        el.value = ccNumberInputOldValue;
-        newCursorPosition = ccNumberInputOldCursor;
-      }
+        el.setSelectionRange(newCursorPosition, newCursorPosition);
+      },
+      // expiry date
+      ccExpiryInputKeyDownHandler = (e) => {
+        let el = e.target;
+        ccExpiryInputOldValue = el.value;
+        ccExpiryInputOldCursor = el.selectionEnd;
+      },
+      ccExpiryInputInputHandler = (e) => {
+        let el = e.target,
+          newValue = el.value;
 
-      el.setSelectionRange(newCursorPosition, newCursorPosition);
-    },
-    // expiry date
-    ccExpiryInputKeyDownHandler = (e) => {
-      let el = e.target;
-      ccExpiryInputOldValue = el.value;
-      ccExpiryInputOldCursor = el.selectionEnd;
-    },
-    ccExpiryInputInputHandler = (e) => {
-      let el = e.target,
-        newValue = el.value;
+        newValue = unmask(newValue);
+        if (newValue.match(ccExpiryPattern)) {
+          newValue = mask(newValue, 2, ccExpirySeparator);
+          el.value = newValue;
+        } else {
+          el.value = ccExpiryInputOldValue;
+        }
+      };
 
-      newValue = unmask(newValue);
-      if (newValue.match(ccExpiryPattern)) {
-        newValue = mask(newValue, 2, ccExpirySeparator);
-        el.value = newValue;
-      } else {
-        el.value = ccExpiryInputOldValue;
-      }
-    };
+    // card number
+    ccNumberInput.addEventListener("keydown", ccNumberInputKeyDownHandler);
+    ccNumberInput.addEventListener("input", ccNumberInputInputHandler);
 
-  // card number
-  ccNumberInput.addEventListener("keydown", ccNumberInputKeyDownHandler);
-  ccNumberInput.addEventListener("input", ccNumberInputInputHandler);
-
-  ccExpiryInput.addEventListener("keydown", ccExpiryInputKeyDownHandler);
-  ccExpiryInput.addEventListener("input", ccExpiryInputInputHandler);
-  // code copied from https://codepen.io/murani/pen/KyVbrp
+    ccExpiryInput.addEventListener("keydown", ccExpiryInputKeyDownHandler);
+    ccExpiryInput.addEventListener("input", ccExpiryInputInputHandler);
+    // code copied from https://codepen.io/murani/pen/KyVbrp
+  }
 }
